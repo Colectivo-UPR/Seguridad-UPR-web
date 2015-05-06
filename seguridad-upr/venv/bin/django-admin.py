3e207f514e6eae5e140e01f61fd9c5ee92172c6b
acarrasquillo <abimael.carrasquillo@gmail.com>
2015-05-06 17:03:15 -0400

    Removed venv

--- seguridad-upr/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/xiomara/Documents/Seguridad-UPR-web/seguridad-upr/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
